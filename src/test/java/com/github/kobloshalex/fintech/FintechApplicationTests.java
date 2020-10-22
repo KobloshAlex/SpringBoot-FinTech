9b26472c74a9518f57a76a3828a325d84f65004c
KobloshAlex <Koblosh.Alex@gmail.com>
2020-10-22 17:39:15 -0400

    init

---
+++ src/test/java/com/github/kobloshalex/fintech/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kobloshalex.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
